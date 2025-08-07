cf0e8227ae3597e2f4caff90bcdd7254b7d0a393
perslaursen <111363418+perslaursen@users.noreply.github.com>
2025-08-07 10:17:23 +0200

    Commit of Database, HowTo and UnitTest
    Commit of Database, HowTo and UnitTest

---
+++ Chap/UnitTest/Unsolved/TestExampleB/TestExampleB/Warrior.cs
@@ -0,0 +1,60 @@
+﻿
+/// <summary>
+/// This class is a very simple representation of a Warrior game character.
+/// At this point, the Warrior only has a Name and some Hit Points. The
+/// Warrior can only receive damage...
+/// </summary>
+public class Warrior
+{
+    /// <summary>
+    /// Returns the name of the Warrior
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Returns the current number of Hit Points for the Warrior
+    /// </summary>
+    public int HitPoints { get; private set; }
+
+    /// <summary>
+    /// Returns true if the Warrior is dead (defined as having zero or negative
+    /// Hit Points), otherwise false.
+    /// </summary>
+    public bool IsDead { get { return HitPoints <= 0; } }
+
+
+    public Warrior(string name, int hitPoints)
+    {
+        // The name must contain at least two non-blank characters
+        if (string.IsNullOrWhiteSpace(name) || name.Length < 2)
+        {
+            throw new ArgumentException("Name must contain at least two characters");
+        }
+
+        // Hit point must initially be positive
+        if (hitPoints <= 0)
+        {
+            throw new ArgumentException("Initial hit points must be positive");
+        }
+
+        Name = name;
+        HitPoints = hitPoints;
+    }
+
+
+    /// <summary>
+    /// Deals the given number of damage points to the Warrior.
+    /// The Warrior's Hit Points are reduced with the given number
+    /// of damage points.
+    /// </summary>
+    public void ReceiveDamage(int damagePoints)
+    {
+        // Damage points must be non-negative
+        if (damagePoints < 0)
+        {
+            throw new ArgumentException("Damage points must be non-negative");
+        }
+
+        HitPoints = HitPoints - damagePoints;
+    }
+}
