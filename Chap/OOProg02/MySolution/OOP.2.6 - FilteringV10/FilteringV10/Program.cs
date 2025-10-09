1f17ad601afaca39921664a31b2126076704da0c
lukasbjohansen <lukasbjohansen@gmail.com>
2025-10-09 23:07:36 +0200

    buttstuff

--- Chap/OOProg02/MySolution/OOP.2.6 - FilteringV10/FilteringV10/Program.cs
+++ Chap/OOProg02/MySolution/OOP.2.6 - FilteringV10/FilteringV10/Program.cs
@@ -1,8 +1,10 @@
 ﻿
 List<int> values = new List<int>() { 12, 24, 9, 10, 6, 3, 45 };
-List<int> filteredValues = Filter.FilterValues(values,ValueOver10.Instance);
+List<int> filteredValues = Filter.FilterValues(values,ValueOver10.Instance, DivisableBy3.Instance);
 
-foreach (var value in filteredValues)
+List<int> netFilteredValues = values.Where(v => v > 10 && v % 2 == 1).ToList();
+
+foreach (var value in netFilteredValues)
 {
     Console.Write($" {value} ");
 }
